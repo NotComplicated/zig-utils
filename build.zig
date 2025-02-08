68f9b9e99deb4b8cdc98432487124a48d63f4b00
Andres Dejesus <andresdejesus123@gmail.com>
2025-02-07 22:05:43 -0600

    second commit :-)

--- build.zig
+++ build.zig
@@ -15,11 +15,13 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
+    const main = "src/main.zig";
+
     const lib = b.addStaticLibrary(.{
         .name = "zig-utils",
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = .{ .src_path = .{ .sub_path = main, .owner = b } },
         .target = target,
         .optimize = optimize,
     });
@@ -32,7 +34,7 @@ pub fn build(b: *std.Build) void {
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const main_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = .{ .src_path = .{ .sub_path = main, .owner = b } },
         .target = target,
         .optimize = optimize,
     });
